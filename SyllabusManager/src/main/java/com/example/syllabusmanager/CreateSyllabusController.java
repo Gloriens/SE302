23009855af9d111a6b41de2b7096214fa150cc8b
Oğuzhan Çalışkan <77392426+Gloriens@users.noreply.github.com>
2023-12-13 20:26:40 +0300

    Create Syllabus Page
    Create Syllabus Page Finished

---
+++ SyllabusManager/src/main/java/com/example/syllabusmanager/CreateSyllabusController.java
@@ -0,0 +1,35 @@
+package com.example.syllabusmanager;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CreateSyllabusController {
+    @FXML
+    private TextField code_TextF_GeneralInf;
+
+
+
+
+    @FXML
+    private void addButtonFunction_CreateSyllabusPage(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void saveallButtonFunction_CreateSyllabus(ActionEvent event) {
+
+    }
+
+
+
+}
