c6febb70fc9300d5c21c7c1a3863408dacf6e3ba
Oğuzhan Çalışkan <77392426+Gloriens@users.noreply.github.com>
2023-12-13 18:52:26 +0300

    CreateSyllabus
    CreateSyllabus fxml page created
    Windows size is now acceptiable

--- SyllabusManager/src/main/java/com/example/syllabusmanager/HomePageController.java
+++ SyllabusManager/src/main/java/com/example/syllabusmanager/HomePageController.java
@@ -2,14 +2,46 @@ package com.example.syllabusmanager;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HomePageController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Button createSyllabusButton;
+
+    @FXML
+    private AnchorPane homePage;
+
+    @FXML
     private void createSyllabusAction(ActionEvent event) {
-        // Metodun içeriği burada olacak
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("CreateSyllabus.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Create Syllabus");
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+
+            stage.setWidth(1300);
+            stage.setHeight(1000);
+
+            stage.setResizable(false);
+
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
